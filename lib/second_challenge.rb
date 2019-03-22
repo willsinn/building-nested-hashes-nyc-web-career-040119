818124c2ac0bcc85de8e7b2ab663bd16361a3691
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 10:04:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-    [:patriarch,:matriarch, :hero, :hero_friends]       
+    [:patriarch => {},:matriarch => {}, :hero => {}, :hero_friends => {}]       
       
     }, 
     :capulet => {
