43a57d2ec3b813dd30fa044f58c44dc1bcd35597
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 10:42:11 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,8 @@ def bonus
   }
 
   #code your solution here:
-
+epic_tragedy[1][:heroine][status:]="dead"
+epic_tragedy
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,6 @@
 def first_challenge
-   epic_tragedy = {}
+   first_challenge_keys = {
+     :montague => {},
+     :capulet => {}
+   }
 end
